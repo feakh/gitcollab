118ce308307915eb8785606015433fc17889a23e
soundn <soundnwankwo@gmail.com>
2024-05-13 11:52:00 +0100

    accepted

--- shell_commands.sh
+++ shell_commands.sh
@@ -1,11 +1,4 @@
 ls -al # list contents of directory
-<<<<<<< HEAD
-cp
-mv
-mkdir 
-grep
-=======
 cp # copy file
 mv # move file
 mkdir # make new directory
->>>>>>> 7852d46c77611b69196151cbdde876eda159e022
